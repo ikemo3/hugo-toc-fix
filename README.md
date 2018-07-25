1924820bb063cfd57561b2f119203e5ea1ba5904
Hideki Ikemoto <ikemo333@gmail.com>
2018-07-25 21:24:55 +0900

    initial version.

---
+++ README.md
@@ -0,0 +1,29 @@
+# hugo-toc-fix
+
+[Hugo][]で、目次を修正するためのテーマです。
+
+[Issue #1778][]で議論されているように、
+現在は`<h1>`から目次を作成するため、本文が`<h2>`から始まっていると動きません。
+これを修正します。
+
+## 前提条件
+
+[Theme Components][]を使用するため、[Hugo][] 0.42以上が必要です。
+
+## サポートしているサイト
+
+## テンプレートへの組み込み方法
+
+テンプレートに以下の記述を追加してください。
+
+```
+{{- partialCached "hugo-toc" . }}
+```
+
+## ライセンス
+
+MITライセンスです。
+
+[Hugo]: https://gohugo.io/
+[Theme Components]: https://gohugo.io/themes/theme-components/
+[Issue #1778]: https://github.com/gohugoio/hugo/issues/1778
---
+++ static/js/hugo-toc.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toc = document.getElementById('TableOfContents');
+
+    if (toc === null) {
+        return;
+    }
+
+    // make heading tree
+    const root = {children: []};
+
+    let h2 = null;
+    let h3 = null;
+    let h4 = null;
+    let h5 = null;
+
+    const headings = document.querySelectorAll("h2, h3, h4, h5, h6");
+    headings.forEach(function (heading) {
+        switch (heading.tagName.toLowerCase()) {
+            case "h2":
+                h2 = {id: heading.getAttribute("id"), title: heading.textContent, children: []};
+                root.children.push(h2);
+                h3 = h4 = h5 = null;
+                break;
+
+            case "h3":
+                h3 = {id: heading.getAttribute("id"), title: heading.textContent, children: []};
+                h2.children.push(h3);
+                h4 = h5 = null;
+                break;
+
+            case "h4":
+                h4 = {id: heading.getAttribute("id"), title: heading.textContent, children: []};
+                h3.children.push(h4);
+                h5 = null;
+                break;
+
+            case "h5":
+                h5 = {id: heading.getAttribute("id"), title: heading.textContent, children: []};
+                h4.children.push(h5);
+                break;
+
+            case "h6":
+                const h6 = {id: heading.getAttribute("id"), title: heading.textContent, children: []};
+                h5.children.push(h6);
+                break;
+        }
+    });
+
+    // tree -> DOM
+    function createToc(element) {
+        if (element.children.length === 0) {
+            return null;
+        }
+
+        const ul = document.createElement("ul");
+
+        element.children.forEach(function (child) {
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+            a.href = "#" + child.id;
+            a.textContent = child.title;
+            li.appendChild(a);
+
+            const childNodes = createToc(child);
+            if (childNodes !== null) {
+                li.appendChild(childNodes);
+            }
+
+            ul.appendChild(li);
+        });
+
+        return ul;
+    }
+
+    toc.innerHTML = "";
+    toc.appendChild(createToc(root));
+});
